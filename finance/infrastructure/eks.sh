a257b7b51ac1f006684d239732a94bbf0da0f38c
CesarOspina <cao.j122@gmail.com>
2025-01-20 23:11:25 -0500

    eks deploy

---
+++ finance/infrastructure/eks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo =====================================
+echo Crear cluster de Kubernetes
+echo =====================================
+eksctl create cluster -f $SYSTEM_DEFAULTWORKINGDIRECTORY/infrastructure/eks.yaml 
+
+sleep 50
+
+echo ============================================================================
+echo Ejecutar aws eks list-clusters
+echo ============================================================================
+aws eks list-clusters
+
+echo ============================================================================
+echo Ejecutar update kubeconfig
+echo ============================================================================
+aws eks update-kubeconfig --name eks-p-poc
+
+echo =====================================
+echo Validar nodos
+echo =====================================
+kubectl get nodes 
+
+echo =====================================
+echo Validar pod
+echo =====================================
+kubectl get po --all-namespaces
