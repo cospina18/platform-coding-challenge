7bb407d4ca9d1fbe0868a9b028e05837afd5dd44
CesarOspina <cao.j122@gmail.com>
2025-01-22 22:48:16 -0500

    fluentd login update

--- finance/web/utilities/services/fluentd_logs.py
+++ finance/web/utilities/services/fluentd_logs.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-from datetime import datetime, timezone
 
 def configure_logging():
     """Configura el logger principal para toda la aplicación."""
@@ -11,9 +10,10 @@ def configure_logging():
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setLevel(logging.DEBUG)
     
-    # Crear un formateador personalizado para usar el formato de hora en UTC
-    formatter = CustomFormatter(
-        '{"time": "%(asctime)s", "level": "%(levelname)s", "logger": "%(name)s", "message": "%(message)s"}'
+    # Crear un formateador estándar con datefmt
+    formatter = logging.Formatter(
+        '{"time": "%(asctime)s", "level": "%(levelname)s", "logger": "%(name)s", "message": "%(message)s"}',
+        datefmt='%Y-%m-%dT%H:%M:%SZ'
     )
     
     # Configurar el formateador al handler
@@ -23,20 +23,6 @@ def configure_logging():
     # Redirigir stdout y stderr a logging
     sys.stdout = StreamToLogger(logger, logging.INFO)
     sys.stderr = StreamToLogger(logger, logging.ERROR)
-
-
-class CustomFormatter(logging.Formatter):
-    """Formateador personalizado para incluir el timestamp en formato UTC"""
-    def formatTime(self, record, datefmt=None):
-        # Obtener la hora actual en UTC y formatear como "%Y-%m-%dT%H:%M:%S"
-        timestamp = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%S')
-        # Añadir 'Z' para indicar UTC
-        return f"{timestamp}Z"
-
-    def format(self, record):
-        # Usar el formato personalizado de hora para el resto del mensaje
-        record.asctime = self.formatTime(record)
-        return super().format(record)
 
 class StreamToLogger:
     """Redirige stdout y stderr al sistema de logging."""
@@ -50,3 +36,5 @@ class StreamToLogger:
 
     def flush(self):
         pass
+
+# Configurar el logging al inicio de tu aplicación
